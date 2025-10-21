801535c80bfaa2a7e2e569fb5491810a8dd662c8
bUxxat-tefma0-rectof <pedrovasco263@gmail.com>
2025-10-20 22:59:04 -0300

    Add configuration settings for bot and Stripe

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+# Configurações do Bot
+BOT_TOKEN = "8464485123:AAGfibOpvx6ASRrcepmQJlZ1GuoAAYml6Ws"
+ADMIN_ID = 6995978182
+
+# Configurações do Stripe
+STRIPE_API_KEY = "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"
+
+# Configurações do Bot
+BOT_USERNAME = "@JOAOSTORE_BOT"
+SUPPORT_USERNAME = "@suporte_joaozinstore"
+MIN_DEPOSIT = 4.00
+BONUS_PERCENTAGE = 0
+
+# URLs
+GROUP_URL = "https://t.me/joaostore_clientes"
+SUPPORT_URL = "https://t.me/suporte_joaozinstore"
